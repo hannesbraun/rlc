97522be3015669c0896646122e8b31ef086a0e3a
Hannes Braun <hannesbraun@mail.de>
2021-09-25 22:31:12 +0200

    Initial import of source

---
+++ Champions.hx
@@ -0,0 +1,36 @@
+class Champions {
+	public static function getRandomChampion():String {
+		return champions[Std.random(champions.length)];
+	}
+
+	public static function getRandomChampions(i:Int):Array<String> {
+		final result = new Array();
+
+        if (i > champions.length) {
+            i = champions.length;
+        }
+
+		while (result.length < i) {
+			final champ = getRandomChampion();
+			if (!result.contains(champ)) {
+				result.push(champ);
+			}
+		}
+
+		return result;
+	}
+
+	public static final champions = [
+		"Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bard", "Blitzcrank", "Brand",
+		"Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal",
+		"Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia",
+		"Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn",
+		"Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite",
+		"Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nocturne",
+		"Nunu & Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renekton",
+		"Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir",
+		"Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle",
+		"Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir",
+		"Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"
+	];
+}
---
+++ Rlc.hx
@@ -0,0 +1,20 @@
+import js.html.Window;
+import js.Browser.document;
+import js.html.URLSearchParams;
+import js.Browser.window;
+
+class Rlc {
+    public static function main():Void {
+        final urlParams = new URLSearchParams(window.location.search);
+        var num = 1;
+        if (urlParams.has("num")) {
+            final parsed = Std.parseInt(urlParams.get("num"));
+            if (parsed != null) {
+                num = parsed;
+            }
+        }
+
+        final elem = document.getElementById("champion");
+        elem.innerText = Champions.getRandomChampions(num).join("\n");
+    }
+}
